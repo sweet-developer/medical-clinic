defe9f44331a35f27ccd24f671f5858c589d035d
mohammedalsamki <momosamak21@gmail.com>
2021-08-15 11:31:09 +0300

    push all the team work

---
+++ js/page4.js
@@ -0,0 +1,64 @@
+
+
+
+// page4
+
+
+function myFunction() {
+
+  document.getElementById("demo").innerHTML = "Dr.. Salman Habash, born in 1963. He completed his secondary education from Terra Sanctuary College in Amman in 1980. He completed his general medicine studies in 1987 with a grade of “very good.” He completed his specialization in ophthalmology in 1991 with distinction. Doctor of Ophthalmology in 1993 for heavy fluids in retinal surgery And the vitreous fluid. Dr. Salman Habash completed courses in Europe and America in refractive surgery and cataract surgery.";
+}
+
+
+
+
+function dactorawin(){
+
+  document.getElementById('awni').innerHTML = "Dr. Awni Abu Halima Member of the American University of Psychiatry, Member of the International Psychiatric Association, Member of the Jordanian Psychiatric Association, Technical and Academic Director of the National Center for Mental Health Hospital - formerly -, Medical Director of Al-Amal Hospital / Al-Qurayyat - Saudi Arabia - previously -";
+
+ }
+
+
+
+ function malek(){
+
+    document.getElementById('malek').innerHTML = " Dr. Malik Al-Gamzawi Internal Medicine Specialist - Jordanian Board Fellow of Cardiovascular Diseases / University of Jordan Bachelor of General Medicine and Surgery / University of Jordan Internal Medicine Specialist at Prince Hamzah Hospital, formerly Member of the Jordanian Cardiology Society";
+
+}
+
+
+
+function imad(){
+
+    document.getElementById('imad').innerHTML = " Dr. Imad Abu Ramh The British Royal College of Surgeons-London The Jordanian Board of Otolaryngology and Surgery Member of the Jordanian Society of Otolaryngology, Head and Neck";
+
+}
+
+
+function Duha(){
+    document.getElementById('Duha').innerHTML =" Dr. Duha Al-Shalaldah Consultant gynecologist, obstetrics, infertility, gynecological beautification, urinary incontinence problems, in addition to following up on pregnant women, childbirth cases, infertility cases, IVF and vaccination, an integrated section for treating other women’s problems such as women’s beautification, postpartum restoration, treatment of dehydration and treatment of urinary incontinence, either through surgery or by means of modern devices and technologies from Plasma and others."
+
+}
+
+function rana(){
+    document.getElementById('rana').innerHTML ="Dr. Rana Al-Khatib is a dentist who graduated from the University of Jordan in 2009. She worked at Al-Hada Military Hospital in Saudi Arabia and obtained high scientific and practical experience. Dr. Rana cooperates with a number of specialists from Jordanian university professors in various specialties and according to the patient’s need. And my work with high quality, with the same level of service in developed western countries, and at a fair price "
+}
+
+
+function mhanad(){
+    document.getElementById('mhanad').innerHTML ="Dr. Muhannad Al Zoubi Orthopedic and joint surgery specialist. He holds the Jordanian Board Certificate in Orthopedic and Joint Surgery, the Arab Board and the European Board. Dr. Muhannad is a member of the American Academy of Orthopedic Surgery, in addition to being a member of the Jordanian Orthopedic Association. He specializes in the treatment of orthopedic diseases and surgery, fractures, joint replacement, treatment of sports injuries, treatment by endoscopy, early diagnosis of childbirth dislocation through ultrasound, and pediatric orthopedic diseases, in addition to the treatment of friction and joint roughness.."
+
+}
+
+
+
+function mohmad(){
+    document.getElementById('mohmad').innerHTML ="Dr. Mohamed Abu Samak American Fellowship - Henry Ford Hospital Michigan, British Fellowship Glasgow British Fellowship Edinburgh Former Assistant Professor at the College of Medicine and University Hospital of Jordan, Fellow of the European Society of Cataract and Refractive Surgeons, Fellow of the American Society of Retinal Experts."
+
+}
+
+
+// function Qassem(){
+//     document.getElementById('Qassem').innerHTML ="Dr. Qassem has more than 13 years of experience in Britain, starting to work in London in 2006, where he trained in its largest hospital. After that, he moved to work in endoscopic surgery in the well-known city of Bristol and then France. After that, he got a job as a certified consultant in London hospitals. Where he worked for four years. He has several articles published in international journals, in laparoscopic surgery and gynecological urology. Professor of the Faculty of Medicine. Member of the British Jordanian Society. Read more on the doctor's website below. Resident training supervisor in private sector hospitals."}
+
+// page4
